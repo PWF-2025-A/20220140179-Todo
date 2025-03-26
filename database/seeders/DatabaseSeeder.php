59a4ea4d02f0e76532ac6f46be36acb351fed720
dinaamlll <dinamaliaa17@gmail.com>
2025-03-26 18:46:22 +0700

    migration, database seeder, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Todo;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +20,13 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'), 
+            'remember_token' => Str::random(10), 
+            'is_admin' => true,
         ]);
+
+        User::factory(101)->create(); //100 user
+        Todo::factory(500)->create(); //500 todo
     }
 }
