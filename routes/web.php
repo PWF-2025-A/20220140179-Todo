b3803e9187da0103358876db78ecb7758dd9b376
dinaamlll <dinamaliaa17@gmail.com>
2025-04-25 17:21:25 +0700

    create, read, search data

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,11 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
     Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
-    Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
+
 
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
 
+    Route::resource('todo', TodoController::class)->except(['show']);
 });
 
 require __DIR__.'/auth.php';
